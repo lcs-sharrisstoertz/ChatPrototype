927746922389cf8cc7417d5656cfa269b789d111
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-03 08:44:44 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
